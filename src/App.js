e20cb62f7fbc3efaccb328bb8e2aee0148741ae2
nichathong <nicha.fn@gmail.com>
2022-12-06 20:55:55 -0800

    Set bg color

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Footer, Blog, Possibility, Features, WhatBloomyPhy, Header } from './containers';
 import { CTA, Brand, Navbar } from './components';
+import './App.css'
+
 const App = () => {
   return (
     <div className='App'>
