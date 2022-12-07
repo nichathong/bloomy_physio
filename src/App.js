b8addd238b1efe3a8cedd405336f9adbff4fc795
nichathong <nicha.fn@gmail.com>
2022-12-06 20:35:43 -0800

    created file structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+        <h1>Bloomy Physio</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/containers/whatBloomyPhy/WhatBloomyPhy.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./whatbloomyphy.css";
+
+const WhatBloomyPhy = () => {
+  return <div></div>;
+};
+
+export default WhatBloomyPhy;
